b061a857ff6e9cc253e056dca9f62d611d14cf23
Doncho Angelov <public@doncho.net>
2022-02-13 10:28:19 +0200

    Still cannot debug, but reverted to "plain old C++" instead.

--- template/03-with-vscode/main.cpp
+++ template/03-with-vscode/main.cpp
@@ -4,11 +4,5 @@ using namespace std;
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+    cout << "Hello, World!" << endl;
 }
