a82321b661283770317334c7d778d4018dc48dd4
Doncho Angelov <public@doncho.net>
2022-02-13 12:10:54 +0200

    Returning back C++17 code after confirming that was not the debug issue.

--- template/03-with-vscode/main.cpp
+++ template/03-with-vscode/main.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello, World!" << endl;
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
 }
